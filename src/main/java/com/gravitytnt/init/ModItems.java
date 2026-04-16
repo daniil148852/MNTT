88767e04569bcd0ab279b05ab05e8a34d6cd68b1
daniil148852 <daniildan850@gmail.com>
2026-04-16 21:24:42 +0200

    Update ModItems.java

--- src/main/java/com/gravitytnt/init/ModItems.java
+++ src/main/java/com/gravitytnt/init/ModItems.java
@@ -3,14 +3,19 @@ package com.gravitytnt.init;
 import com.gravitytnt.GravityTntMod;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS = 
+    public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, GravityTntMod.MOD_ID);
 
-    public static final RegistryObject<Item> GRAVITY_TNT = ITEMS.register("gravity_tnt", 
+    public static final RegistryObject<Item> GRAVITY_TNT = ITEMS.register("gravity_tnt",
             () -> new BlockItem(ModBlocks.GRAVITY_TNT.get(), new Item.Properties()));
+
+    public static void register(IEventBus bus) {
+        ITEMS.register(bus);
+    }
 }
