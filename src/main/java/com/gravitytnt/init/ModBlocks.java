b765c76556df8fdc0d46da94dde2429da11b6276
daniil148852 <daniildan850@gmail.com>
2026-04-16 21:24:19 +0200

    Update ModBlocks.java

--- src/main/java/com/gravitytnt/init/ModBlocks.java
+++ src/main/java/com/gravitytnt/init/ModBlocks.java
@@ -10,24 +10,36 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS = 
+    public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, GravityTntMod.MOD_ID);
 
-    public static final RegistryObject<Block> GRAVITY_TNT = BLOCKS.register("gravity_tnt", 
+    public static final RegistryObject<Block> GRAVITY_TNT = BLOCKS.register("gravity_tnt",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.TNT)) {
                 @Override
                 public void onPlace(BlockState pState, Level pLevel, BlockPos pPos, BlockState pOldState, boolean pIsMoving) {
                     if (!pLevel.isClientSide) {
-                        GravityTntEntity tnt = new GravityTntEntity(pLevel, pPos.getX() + 0.5D, pPos.getY(), pPos.getZ() + 0.5D, null);
+                        GravityTntEntity tnt = new GravityTntEntity(
+                                pLevel,
+                                pPos.getX() + 0.5D,
+                                pPos.getY(),
+                                pPos.getZ() + 0.5D,
+                                null
+                        );
                         pLevel.addFreshEntity(tnt);
-                        pLevel.playSound(null, tnt.getX(), tnt.getY(), tnt.getZ(), SoundEvents.TNT_PRIMED, SoundSource.BLOCKS, 1.0F, 0.5F);
+                        pLevel.playSound(null, tnt.getX(), tnt.getY(), tnt.getZ(),
+                                SoundEvents.TNT_PRIMED, SoundSource.BLOCKS, 1.0F, 0.5F);
                         pLevel.removeBlock(pPos, false);
                     }
                 }
             });
+
+    public static void register(IEventBus bus) {
+        BLOCKS.register(bus);
+    }
 }
